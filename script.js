497ed3fee18c8b8f6bf7616b77cb50aedf05273d
Abdulaziz <abdusattorova86@gmail.com>
2022-03-05 18:29:34 -1100

    first commit

---
+++ script.js
@@ -0,0 +1,132 @@
+"use strict";
+
+// let number = 3;
+// console.log(number);
+// number = 5;
+// console.log(number);
+
+// const pi = 3.14;
+
+// const obj = {
+//     a:10,
+    
+// }
+// obj.a = 20;
+// console.log(obj);
+
+// var x = 15;
+// console.log(15);
+
+//************** 6 -dars object************************************** */
+
+// const number = 8; //data type = number
+// const firstname = "Abdulaziz"; //string
+// const bool = false; //boolean
+// const isMarried = false;
+
+// console.log(a);//data-type = null
+
+// console.log(4 / 0);//infinity
+
+// console.log("aziz" * 10);// NAN = NOT A NUMBER
+
+// let lastName;
+// console.log(lastName);
+
+// object start
+
+// const person = {
+//     firstname: "Andulaziz",
+//     lastName: "Abdusattorov",
+//     age: 18,
+//     job: "full-stack developer",
+//     isMarried: false,
+// };
+// console.log(person);
+
+// object end
+
+// massiv start
+
+// const arr = ["red", "green", "yellow"];
+// console.log(arr[1]);
+
+// massiv end
+
+//**************  7-dars. Foydalanuvchi bilan oddiy muloqot ************************************** */
+
+// console.log("hello");
+
+// alert("hello");
+
+// const userAge  = confirm("siz 18 yoshdamsz");
+// console.log(userAge);
+
+// const userName = prompt("Ismiz nima", "ismingizni kriting");
+// alert(userName);
+
+// const answers = [];
+// answers[0] = prompt('ismiz nima');
+// answers[1] = prompt('familya nima');
+// answers[2] = prompt('yoshiz nechchida');
+// console.log(answers);
+
+// const firsName = +prompt("yoshiz", "");
+// console.log(typeof firsName + 10);
+
+
+//**************   8-dars. Interpolatsiya (ES-6) ************************************** */  
+// const firsName = "Abdulaziz";
+// const age = 19;
+
+// console.log("mani ismim" + firsName + "va yoshm" + age);
+
+// alert(`mani ismim ${firsName} va yoshim${age}` );
+
+// const xName = prompt("Enter your name", "");
+// console.log(`user's name${xName}`);
+
+//**************   9-dars. JSdagi operatorlar ************************************** */  
+//     const x=10,
+// y=15;
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(5/5);
+// console.log(5*5);
+
+// console.log("Hello" + "" + "World");
+// const num = 4 + "radio";
+// console.log(typeof num);
+// console.log(4 + "radio");
+
+// let z = 10;
+// console.log(++z);
+
+// let incr = 99;
+// let decr = 99;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5 % 2);
+// const q = 15,
+// m = 7;
+// console.log(q % m);
+// console.log(2 * 3 == 6);//true znachenya
+// console.log(2 * 3 === 6);//false malumot turi + znachenya
+
+//     // && va
+//     // || yoki
+//     // ! yoq masalan true bo'lsa false qlb beradi
+
+
+
+// const isChacked = true,
+// isClose = false;
+// console.log(isChacked && (isClose || isChacked) );
+
+//***  10-dars Git versiyasini boshqarish tizimi va GitHub xizmati bilan ishlashni o’rganish  ****/  
